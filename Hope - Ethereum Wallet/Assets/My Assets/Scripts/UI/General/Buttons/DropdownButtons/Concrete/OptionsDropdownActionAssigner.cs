ae12c6a2789f2cb9f25e33c8b4e081e21b5c5a9b
Matthew LeLiever <matthew121250@hotmail.com>
2018-06-17 22:38:53 -0400

    Comments

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Buttons/DropdownButtons/Concrete/OptionsDropdownActionAssigner.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Buttons/DropdownButtons/Concrete/OptionsDropdownActionAssigner.cs
@@ -6,6 +6,11 @@
 public class OptionsDropdownActionAssigner
 {
 
+    /// <summary>
+    /// Adds all the actions for the options dropdowns.
+    /// </summary>
+    /// <param name="uiSettings"> The ui settings which contains the dropdown info. </param>
+    /// <param name="popupManager"> The active PopupManager. </param>
     public OptionsDropdownActionAssigner(UIManager.Settings uiSettings, PopupManager popupManager)
     {
         uiSettings.generalSettings.dropdowns.extraOptionsDropdowns[0].onClickAction = () => popupManager.GetPopup<PRPSHodlPopup>();
