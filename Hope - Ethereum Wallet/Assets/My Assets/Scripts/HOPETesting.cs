203118fa99ae47ab3e919696ab8ebd685bc3652e
Matthew LeLiever <matthew121250@hotmail.com>
2018-06-25 13:15:42 -0400

    Removed Ledger api files
    - Only Bitcoin app is supported, need to investigate a solution later on

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/HOPETesting.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/HOPETesting.cs
@@ -1,23 +1,14 @@
-﻿using LedgerWallet;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using Zenject;
+﻿using UnityEngine;
 
 public class HOPETesting : MonoBehaviour
 {
 
     private void Start()
     {
-        var ledger = LedgerClient.GetHIDLedgers().First();
-        Debug.Log(ledger);
     }
 
     private void Update()
-    {
-        
+    {   
     }
 
 }
