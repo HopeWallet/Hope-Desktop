c480c9e1e0366d0eab892fa8d9f2aa845ceb357d
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-06-26 21:09:28 -0400

    New ui first commit

---
+++ Hope - Ethereum Wallet/Assets/UI/Scripts/ChooseWalletMenu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class ChooseWalletMenu : MenuAnimation
+{
+
+	[SerializeField] private GameObject vignette;
+	[SerializeField] private GameObject form;
+	[SerializeField] private GameObject title;
+	[SerializeField] private GameObject ledgerButton;
+	[SerializeField] private GameObject hopeButton;
+
+	#region Animating
+
+	/// <summary>
+	/// Animates the form's UI Elements into display
+	/// </summary>
+	protected override void AnimateIn()
+	{
+		vignette.AnimateGraphic(1f, 0.5f);
+		form.AnimateGraphicAndScale(1f, 1f, 0.2f,
+			() => title.AnimateGraphicAndScale(0.85f, 1f, 0.2f,
+			() => ledgerButton.AnimateGraphicAndScale(1f, 1f, 0.2f,
+			() => hopeButton.AnimateGraphicAndScale(1f, 1f, 0.2f, 
+			() => FinishedAnimatingIn()))));
+	}
+
+	/// <summary>
+	/// Animates the form's UI Elements out of display
+	/// </summary>
+	protected override void AnimateOut()
+	{
+		hopeButton.AnimateGraphicAndScale(0f, 0.1f, 0.2f);
+		ledgerButton.AnimateGraphicAndScale(0f, 0.1f, 0.2f);
+		title.AnimateGraphicAndScale(0f, 0.1f, 0.2f,
+			() => form.AnimateGraphicAndScale(0f, 0.1f, 0.2f,
+			() => FinishedAnimatingOut()));
+	}
+
+	#endregion
+
+	/// <summary>
+	/// Disables the menu and opens up the hope wallets menu, or ledger menu
+	/// </summary>
+	/// <param name="hopeWalletClicked">Checks to see if the hope wallet button has been clicked</param>
+	public void ButtonClicked(bool hopeWalletClicked)
+	{
+		DisableMenu();
+
+		//if (hopeWalletClicked)
+			//Add Wallets menu next
+
+		//else 
+			//Show ledger menu next
+	}
+
+}
---
+++ Hope - Ethereum Wallet/Assets/UI/Scripts/MenuAnimator.cs
@@ -0,0 +1,45 @@
+﻿using DG.Tweening;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class MenuAnimator
+{
+
+	private readonly static Dictionary<GameObject, List<Component>> animatedComponents = new Dictionary<GameObject, List<Component>>();
+
+	public static void AnimateGraphicAndScale(this GameObject gameObject, float alpha, float scale, float duration, TweenCallback callback = null)
+	{
+		gameObject.AnimateGraphic(alpha, duration);
+		gameObject.AnimateScaleX(scale, duration);
+		gameObject.AnimateScaleY(scale, duration, callback);
+	}
+
+	public static void AnimateGraphic(this GameObject gameObject, float endValue, float duration, TweenCallback callback = null)
+	{
+		gameObject.GetAnimationComponent<Graphic>().DOFade(endValue, duration).OnComplete(() => callback?.Invoke());
+	}
+
+	public static void AnimateScaleX(this GameObject gameObject, float endValue, float duration, TweenCallback callback = null)
+	{
+		gameObject.GetAnimationComponent<Transform>().DOScaleX(endValue, duration).OnComplete(() => callback?.Invoke());
+	}
+
+	public static void AnimateScaleY(this GameObject gameObject, float endValue, float duration, TweenCallback callback = null)
+	{
+		gameObject.GetAnimationComponent<Transform>().DOScaleY(endValue, duration).OnComplete(() => callback?.Invoke());
+	}
+
+	public static T GetAnimationComponent<T>(this GameObject gameObject) where T : Component
+	{
+		if (!animatedComponents.ContainsKey(gameObject))
+			animatedComponents.Add(gameObject, new List<Component>());
+
+		if (animatedComponents[gameObject].OfType<T>().Count() == 0)
+			animatedComponents[gameObject].Add(gameObject.GetComponent<T>());
+
+		return animatedComponents[gameObject].OfType<T>().FirstOrDefault();
+	}
+
+}
