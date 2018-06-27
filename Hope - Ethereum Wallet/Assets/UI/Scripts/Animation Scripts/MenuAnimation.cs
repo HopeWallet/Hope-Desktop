7945ba0efabe3f59be5752d50bc0391e44ebb22d
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-06-27 16:38:40 -0400

    New Menu Added
    - Added the HopeWallet_Menu prefab with the animating in and out

---
+++ Hope - Ethereum Wallet/Assets/UI/Scripts/Animation Scripts/MenuAnimation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public abstract class MenuAnimation : MonoBehaviour
+{
+
+	public GameObject blocker;
+
+	public bool Animating { get; private set; }
+
+	protected void DisableMenu()
+	{
+		ChangeAnimationState(true);
+		AnimateOut();
+	}
+
+	private void OnEnable()
+	{
+		ChangeAnimationState(true);
+		AnimateIn();
+	}
+
+	protected abstract void AnimateIn();
+
+	protected abstract void AnimateOut();
+
+	protected void FinishedAnimatingIn()
+	{
+		ChangeAnimationState(false);
+	}
+
+	protected void FinishedAnimatingOut()
+	{
+		ChangeAnimationState(false);
+		//Destroy(this.gameObject);
+	}
+
+	private void ChangeAnimationState(bool state)
+	{
+		blocker.SetActive(state);
+		Animating = state;
+	}
+
+}
---
+++ Hope - Ethereum Wallet/Assets/UI/Scripts/Menu Scripts/HopeWalletMenu.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class HopeWalletMenu : MenuAnimation
+{
+	[SerializeField] private GameObject form;
+	[SerializeField] private GameObject title;
+	[SerializeField] private GameObject importWalletButton;
+	[SerializeField] private GameObject importWalletDesc;
+	[SerializeField] private GameObject createWalletButton;
+	[SerializeField] private GameObject createWalletDesc;
+
+	protected override void AnimateIn()
+	{
+		form.AnimateGraphicAndScale(1f, 1f, 0.2f,
+			() => title.AnimateGraphicAndScale(0.85f, 1f, 0.2f));
+
+		importWalletButton.AnimateScaleX(1f, 0.2f,
+			() => importWalletDesc.AnimateScaleX(1f, 0.2f,
+			() => createWalletButton.AnimateScaleX(1f, 0.2f,
+			() => createWalletDesc.AnimateScaleX(1f, 0.2f,
+			() => FinishedAnimatingIn()))));
+	}
+
+	protected override void AnimateOut()
+	{
+		createWalletDesc.AnimateScaleX(0f, 0.1f,
+			() => createWalletButton.AnimateScaleX(0f, 0.1f));
+
+		importWalletDesc.AnimateScaleX(0f, 0.1f,
+			() => importWalletButton.AnimateScaleX(0f, 0.1f,
+			() => title.AnimateGraphicAndScale(0f, 0f, 0.15f,
+			() => form.AnimateGraphicAndScale(0f, 0f, 0.15f,
+			() => FinishedAnimatingOut()))));
+	}
+
+	/// <summary>
+	/// Disables the menu and proceeds to next menu
+	/// </summary>
+	/// <param name="importing">Checks if user is importing wallet or creating new wallet</param>
+	public void ImportOrCreateClicked(bool importing)
+	{
+		DisableMenu();
+
+		//if (import)
+			//Open up importing wallet menu
+
+		//else
+			//Open up creating wallet menu
+	}
+
+
+	/// <summary>
+	/// Disables menu and moves to previous menu
+	/// </summary>
+	public void BackButton()
+	{
+		DisableMenu();
+
+		//Go back to saved wallets menu, or choose wallet menu, depending on if first time or not
+	}
+}
