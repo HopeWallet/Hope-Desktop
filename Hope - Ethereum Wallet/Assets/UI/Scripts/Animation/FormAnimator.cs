666beae45db86f3fd4666e5a6e96c38e821b692d
Matthew LeLiever <matthew121250@hotmail.com>
2018-06-30 02:52:31 -0400

    Small optimization fixes

--- Hope - Ethereum Wallet/Assets/UI/Scripts/Animation/FormAnimator.cs
+++ Hope - Ethereum Wallet/Assets/UI/Scripts/Animation/FormAnimator.cs
@@ -1,13 +1,9 @@
 ﻿using DG.Tweening;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public static class FormAnimator
 {
-
-	private readonly static Dictionary<GameObject, List<Component>> animatedComponents = new Dictionary<GameObject, List<Component>>();
 
 	public static void AnimateGraphicAndScale(this GameObject gameObject, float alpha, float scale, float duration, TweenCallback callback = null)
 	{
@@ -18,28 +14,17 @@ public static class FormAnimator
 
 	public static void AnimateGraphic(this GameObject gameObject, float endValue, float duration, TweenCallback callback = null)
 	{
-		gameObject.GetAnimationComponent<Graphic>().DOFade(endValue, duration).OnComplete(() => callback?.Invoke());
+		gameObject.GetComponent<Graphic>().DOFade(endValue, duration).OnComplete(() => callback?.Invoke());
 	}
 
 	public static void AnimateScaleX(this GameObject gameObject, float endValue, float duration, TweenCallback callback = null)
 	{
-		gameObject.GetAnimationComponent<Transform>().DOScaleX(endValue, duration).OnComplete(() => callback?.Invoke());
+		gameObject.transform.DOScaleX(endValue, duration).OnComplete(() => callback?.Invoke());
 	}
 
 	public static void AnimateScaleY(this GameObject gameObject, float endValue, float duration, TweenCallback callback = null)
 	{
-		gameObject.GetAnimationComponent<Transform>().DOScaleY(endValue, duration).OnComplete(() => callback?.Invoke());
-	}
-
-	public static T GetAnimationComponent<T>(this GameObject gameObject) where T : Component
-	{
-		if (!animatedComponents.ContainsKey(gameObject))
-			animatedComponents.Add(gameObject, new List<Component>());
-
-		if (animatedComponents[gameObject].OfType<T>().Count() == 0)
-			animatedComponents[gameObject].Add(gameObject.GetComponent<T>());
-
-		return animatedComponents[gameObject].OfType<T>().FirstOrDefault();
+		gameObject.transform.DOScaleY(endValue, duration).OnComplete(() => callback?.Invoke());
 	}
 
 }
