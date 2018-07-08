3f51830faa3aec26d002e0381a9d522b7ae23afe
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-07-07 20:22:17 -0400

    More features on OpenWalletForm

--- Hope - Ethereum Wallet/Assets/UI/Scripts/Menu Forms/OpenWalletForm.cs
+++ Hope - Ethereum Wallet/Assets/UI/Scripts/Menu Forms/OpenWalletForm.cs
@@ -1,37 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class OpenWalletForm : MonoBehaviour {
+public class OpenWalletForm : MonoBehaviour
+{
 
-	[SerializeField] private GameObject token1;
-	[SerializeField] private GameObject token2;
-	[SerializeField] private GameObject token3;
 
-	private void Start()
-	{
-		token1.GetComponent<Button>().onClick.AddListener(Button1Clicked);
-		token2.GetComponent<Button>().onClick.AddListener(Button2Clicked);
-		token3.GetComponent<Button>().onClick.AddListener(Button3Clicked);
-	}
-
-	private void Button1Clicked()
-	{
-		token1.GetComponent<Button>().interactable = false;
-		token2.GetComponent<Button>().interactable = true;
-		token3.GetComponent<Button>().interactable = true;
-	}
-
-	private void Button2Clicked()
-	{
-		token1.GetComponent<Button>().interactable = true;
-		token2.GetComponent<Button>().interactable = false;
-		token3.GetComponent<Button>().interactable = true;
-	}
-
-	private void Button3Clicked()
-	{
-		token1.GetComponent<Button>().interactable = true;
-		token2.GetComponent<Button>().interactable = true;
-		token3.GetComponent<Button>().interactable = false;
-	}
 }
