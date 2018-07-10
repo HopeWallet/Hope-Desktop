083ee7d9efc37a21f42cc12b825beaaeeaf79b47
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-07-10 18:53:55 -0400

    More animations
    - LoadingBar form and VerifyingPassword form both have AnimateIn and AnimatOut
    - Both forms also have other cool animations going on while it loads

---
+++ Hope - Ethereum Wallet/Assets/UI/Scripts/Menu Forms/LoadingBarForm.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class LoadingBarForm : UIAnimator
+{
+
+	[SerializeField] private GameObject dim;
+	[SerializeField] private GameObject form;
+	[SerializeField] private GameObject text;
+	[SerializeField] private GameObject loadingBar;
+
+	private string mainText = "Loading";
+
+	/// <summary>
+	/// The main text object's string
+	/// </summary>
+	private string MainText
+	{
+		get { return mainText; }
+
+		set
+		{
+			mainText = value;
+			text.GetComponent<TextMeshProUGUI>().text = value;
+		}
+	}
+
+
+	/// <summary>
+	/// Starts the coroutine
+	/// </summary>
+	private void Start()
+	{
+		StartCoroutine(AddDots());
+	}
+
+	/// <summary>
+	/// Animates the UI elements of the form into view
+	/// </summary>
+	protected override void AnimateIn()
+	{
+		dim.AnimateGraphic(1f, 0.1f,
+			() => form.AnimateGraphicAndScale(1f, 1f, 0.1f,
+			() => text.AnimateScaleX(1f, 0.1f,
+			() => loadingBar.AnimateScaleX(1f, 0.1f, FinishedAnimating))));
+	}
+
+	/// <summary>
+	/// Animates the UI elements of the form out of view
+	/// </summary>
+	protected override void AnimateOut()
+	{
+		loadingBar.AnimateScaleX(0f, 0.1f,
+			() => text.AnimateScaleX(0f, 0.1f,
+			() => form.AnimateGraphicAndScale(0f, 0f, 0.1f,
+			() => dim.AnimateGraphic(0f, 0.1f, FinishedAnimating))));
+	}
+
+	/// <summary>
+	/// Animates the dots in the string of the text object
+	/// </summary>
+	/// <returns></returns>
+	private IEnumerator AddDots()
+	{
+		yield return new WaitForSeconds(0.3f);
+
+		if (MainText == "Loading....")
+			MainText = "Loading";
+		else
+			MainText += ".";
+
+		StartCoroutine(AddDots());
+	}
+}
---
+++ Hope - Ethereum Wallet/Assets/UI/Scripts/Menu Forms/VerifyingPasswordForm.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class VerifyingPasswordForm : UIAnimator
+{
+
+	[SerializeField] private GameObject dim;
+	[SerializeField] private GameObject form;
+	[SerializeField] private GameObject text;
+	[SerializeField] private GameObject loadingIcon;
+
+	private string mainText = "Verifying Password";
+
+	/// <summary>
+	/// The main text object's string
+	/// </summary>
+	private string MainText
+	{
+		get { return mainText; }
+
+		set
+		{
+			mainText = value;
+			text.GetComponent<TextMeshProUGUI>().text = value;
+		}
+	}
+
+	/// <summary>
+	/// Spins the loadingIcon around in a circle
+	/// </summary>
+	private void Update()
+	{
+		loadingIcon.transform.DOLocalRotate(new Vector3(0f, 0f, -360f), 5f, RotateMode.LocalAxisAdd);
+	}
+
+	/// <summary>
+	/// Starts the coroutine
+	/// </summary>
+	private void Start()
+	{
+		StartCoroutine(AddDots());
+	}
+
+	/// <summary>
+	/// Animates the UI elements of the form into view
+	/// </summary>
+	protected override void AnimateIn()
+	{
+		dim.AnimateGraphic(1f, 0.1f,
+			() => form.AnimateGraphicAndScale(1f, 1f, 0.1f,
+			() => text.AnimateScaleX(1f, 0.1f,
+			() => loadingIcon.AnimateGraphicAndScale(1f, 0.75f, 0.1f, FinishedAnimating))));
+	}
+
+	/// <summary>
+	/// Animates the UI elements of the form out of view
+	/// </summary>
+	protected override void AnimateOut()
+	{
+		loadingIcon.AnimateGraphicAndScale(0f, 0f, 0.1f,
+			() => text.AnimateScaleX(0f, 0.1f,
+			() => form.AnimateGraphicAndScale(0f, 0f, 0.1f,
+			() => dim.AnimateGraphic(0f, 0.1f, FinishedAnimating))));
+	}
+
+	/// <summary>
+	/// Animates the dots in the string of the text object
+	/// </summary>
+	/// <returns></returns>
+	private IEnumerator AddDots()
+	{
+		yield return new WaitForSeconds(0.3f);
+
+		if (MainText == "Verifying Password....")
+			MainText = "Verifying Password";
+		else
+			MainText += ".";
+
+		StartCoroutine(AddDots());
+	}
+}
