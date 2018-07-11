b2af2bb9ffbb9223228a4385ad678ff0ea5ad940
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-07-11 00:36:01 -0400

    Added a different type of loading icon/animation: LoadingLine prefab

---
+++ Hope - Ethereum Wallet/Assets/UI/Scripts/LoadingLine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadingLine : MonoBehaviour
+{
+
+	[SerializeField] private GameObject[] circles;
+
+	void Start() => StartCoroutine(MoveCircles());
+
+	private IEnumerator MoveCircles()
+	{
+		yield return new WaitForSeconds(0.1f);
+
+		for (int i = 0; i < circles.Length; i++)
+		{
+			Vector2 circleTransform = circles[i].transform.localPosition;
+			circles[i].transform.localPosition = new Vector2(circleTransform.x >= 100 ? -100f : circleTransform.x + 25f, 0f);
+		}
+
+		StartCoroutine(MoveCircles());
+	}
+}
