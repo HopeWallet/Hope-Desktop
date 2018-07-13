e834b6078ff3e762cc314037bfe08596a8bb678b
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-07-13 14:31:52 -0400

    Small fixes and additions

---
+++ Hope - Ethereum Wallet/Assets/UI/Scripts/CheckBoxAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckBoxAnimator : MonoBehaviour
+{
+
+	private Button checkBoxButton;
+	private GameObject checkMarkIcon;
+
+	private bool toggledOn;
+
+	public bool ToggledOn
+	{
+		get { return toggledOn; }
+		set { toggledOn = value; }
+	}
+
+	private void Awake()
+	{
+		checkBoxButton = transform.GetComponent<Button>();
+		checkBoxButton.onClick.AddListener(CheckBoxClicked);
+		checkMarkIcon = transform.GetChild(0).gameObject;
+		toggledOn = checkMarkIcon.transform.localScale.x > 0 ? true : false;
+	}
+
+	private void CheckBoxClicked()
+	{
+		checkMarkIcon.AnimateGraphicAndScale(toggledOn ? 0f : 1f, toggledOn ? 0f : 1f, 0.15f);
+		toggledOn = !toggledOn;
+	}
+}
