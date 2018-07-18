f6bf882ca855853ba1dbbebda096343714dba64b
Matthew LeLiever <matthew121250@hotmail.com>
2018-07-18 00:09:23 -0400

    Starting to implement anti reflection which will not execute certain methods which were called through .net reflection

---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Security/Anti Reflection/ReflectionProtect.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+/// <summary>
+/// Attribute to be placed on private methods which want protection against the potential to be called using .NET Reflection.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class ReflectionProtect : Attribute
+{
+}
---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Security/Anti Reflection/RuntimeMethodSearcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+public static class RuntimeMethodSearcher
+{
+
+    private static readonly string[] REFLECTION_CALLS = new string[] { "Invoke", "InternalInvoke" };
+
+    public static bool FindReflectionCalls()
+    {
+        StackTrace stackTrace = new StackTrace();
+
+        for (int i = 0; ; i++)
+        {
+            string methodName = stackTrace.GetFrame(i)?.GetMethod()?.Name;
+
+            if (methodName == null)
+                return false;
+
+            if (REFLECTION_CALLS.ContainsIgnoreCase(methodName))
+                return true;
+        }
+    }
+}
