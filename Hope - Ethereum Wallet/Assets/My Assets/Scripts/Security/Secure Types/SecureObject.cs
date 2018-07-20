2b5884acc79ac1dbbba0af5bff5f9fe0a3e1c377
Matthew LeLiever <matthew121250@hotmail.com>
2018-07-20 14:31:10 -0400

    Comments

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/Security/Secure Types/SecureObject.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Security/Secure Types/SecureObject.cs
@@ -2,23 +2,51 @@
 using System.Linq;
 using System.Runtime.CompilerServices;
 
+/// <summary>
+/// Class which overrides the default object methods which restrict calls to only SecureCallers and non reflection calls.
+/// </summary>
 public abstract class SecureObject
 {
-
-    [ReflectionProtect(typeof(int))]
+    /// <summary>
+    /// Gets the hashcode of this object.
+    /// Cannot get the hashcode if the calling method is not secure or if called through reflection.
+    /// </summary>
+    /// <returns> The runtime and base hashcode of this object. </returns>
     [SecureCaller]
+    [ReflectionProtect(typeof(int))]
     public override int GetHashCode()
     {
         return IsSecureCall() ? base.GetHashCode() + RuntimeHelpers.GetHashCode(this) : default(int);
     }
 
-    [ReflectionProtect(typeof(int))]
+    /// <summary>
+    /// Gets the string representation of this object.
+    /// Cannot get the string representation if the calling method is not secure or if called through reflection.
+    /// </summary>
+    /// <returns> The string representation of this object. </returns>
     [SecureCaller]
+    [ReflectionProtect(typeof(int))]
     public override string ToString()
     {
         return IsSecureCall() ? base.ToString() : null;
     }
 
+    /// <summary>
+    /// Gets the type of this object.
+    /// Cannot get the type if the calling method is not secure or if called through reflection.
+    /// </summary>
+    /// <returns> The type of this object. </returns>
+    [SecureCaller]
+    [ReflectionProtect(typeof(Type))]
+    public new Type GetType()
+    {
+        return IsSecureCall() ? base.GetType() : null;
+    }
+
+    /// <summary>
+    /// Checks if the caller methods are secure.
+    /// </summary>
+    /// <returns> True if the line of methods calling this method are secure. </returns>
     [ReflectionProtect(typeof(bool))]
     protected bool IsSecureCall()
     {
