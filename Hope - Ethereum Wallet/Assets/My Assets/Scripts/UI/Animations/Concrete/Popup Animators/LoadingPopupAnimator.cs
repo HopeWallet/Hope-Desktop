b11084b39e5b9c4f4f79971123ec069f030cb892
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-07-26 23:51:04 -0400

    Fixed LoadingPopup animation

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Animations/Concrete/Popup Animators/LoadingPopupAnimator.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Animations/Concrete/Popup Animators/LoadingPopupAnimator.cs
@@ -3,30 +3,13 @@
 public class LoadingPopupAnimator : UIAnimator
 {
 
-	[SerializeField] private GameObject dim;
-	[SerializeField] private GameObject form;
-	[SerializeField] private GameObject text;
-	[SerializeField] private GameObject loadingIcon;
-
 	/// <summary>
 	/// Animates the UI elements of the form into view
 	/// </summary>
-	protected override void AnimateIn()
-	{
-		dim.AnimateGraphic(1f, 0.1f,
-			() => form.AnimateGraphicAndScale(1f, 1f, 0.1f,
-			() => text.AnimateScaleX(1f, 0.1f,
-			() => loadingIcon.AnimateGraphicAndScale(1f, 0.75f, 0.1f, FinishedAnimating))));
-	}
+	protected override void AnimateIn() => gameObject.AnimateGraphicAndScale(1f, 1f, 0.1f, FinishedAnimating);
 
 	/// <summary>
 	/// Animates the UI elements of the form out of view
 	/// </summary>
-	protected override void AnimateOut()
-	{
-		loadingIcon.AnimateGraphicAndScale(0f, 0f, 0.1f,
-			() => text.AnimateScaleX(0f, 0.1f,
-			() => form.AnimateGraphicAndScale(0f, 0f, 0.1f,
-			() => dim.AnimateGraphic(0f, 0.1f, FinishedAnimating))));
-	}
+	protected override void AnimateOut() => gameObject.AnimateGraphicAndScale(0f, 0f, 0.1f, FinishedAnimating);
 }
