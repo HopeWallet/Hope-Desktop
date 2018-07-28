a80e207f9aca9449a231170dcb128d37d34c426b
ThatSlyGuy <matthew121250@hotmail.com>
2018-07-28 02:14:45 -0400

    Comments

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/Editor/Code Modifiers/Helpers/ReflectionHelper.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Editor/Code Modifiers/Helpers/ReflectionHelper.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
+/// <summary>
+/// Class which has some helpful reflection methods that relate to attributes.
+/// </summary>
 public static class ReflectionHelper
 {
 
+    /// <summary>
+    /// Invokes all static methods of a certain type if it contains a specific attribute.
+    /// </summary>
+    /// <typeparam name="T"> The type of attribute to invoke methods for. </typeparam>
+    /// <param name="type"> The class type to search for attributes. </param>
+    /// <param name="parameters"> The method parameters of the methods to invoke. </param>
     public static void InvokeAttributeMethod<T>(Type type, object[] parameters) where T : Attribute
     {
         foreach (var method in type.GetMethods())
@@ -13,6 +19,13 @@ public static class ReflectionHelper
                 method.Invoke(null, parameters);
     }
 
+    /// <summary>
+    /// Checks a type if it contains methods with two different attributes.
+    /// </summary>
+    /// <typeparam name="T"> The type of the first attribute to check for. </typeparam>
+    /// <typeparam name="V"> The type of the second attribute to check for. </typeparam>
+    /// <param name="type"> The class type to search for methods with the attribute types. </param>
+    /// <returns> True if the class type contains methods with all attribute types. </returns>
     public static bool HasMethodAttributes<T, V>(Type type)
     {
         bool hasT = false, hasV = false;
@@ -25,5 +38,4 @@ public static class ReflectionHelper
 
         return hasT && hasV;
     }
-
 }
