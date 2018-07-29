f8a0170489467b4f287e82662bcbc41b5ed1d44d
Matthew LeLiever <matthew121250@hotmail.com>
2018-07-29 19:25:04 -0400

    Added LockPRPSButton

---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Buttons/Regular Buttons/Concrete/LockPRPSButton.cs
@@ -0,0 +1,29 @@
+﻿using Zenject;
+
+/// <summary>
+/// Class used for the Lock Purpose button in the main OpenWalletMenu.
+/// </summary>
+public sealed class LockPRPSButton : ButtonBase
+{
+    private PopupManager popupManager;
+    private LockedPRPSManager lockedPRPSManager;
+
+    /// <summary>
+    /// Adds the dependencies to the LockPRPSButton.
+    /// </summary>
+    /// <param name="popupManager"> The active PopupManager. </param>
+    /// <param name="lockedPRPSManager"> The active LockedPRPSManager. </param>
+    [Inject]
+    public void Construct(PopupManager popupManager, LockedPRPSManager lockedPRPSManager)
+    {
+        this.popupManager = popupManager;
+        this.lockedPRPSManager = lockedPRPSManager;
+    }
+
+    /// <summary>
+    /// Opens the LockPRPSPopup if no purpose has been locked yet, otherwise opens the LockedPRPSPopup.
+    /// </summary>
+    public override void ButtonLeftClicked()
+    {
+    }
+}
