66fa42eefac79dfa4cbc89289b33180cedfe5295
Matthew LeLiever <matthew121250@hotmail.com>
2018-08-06 19:33:03 -0400

    Updates

---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Security/Secure Types/SecurePlayerPrefs/Data Collections/SecurePlayerPrefsList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public sealed class SecurePlayerPrefsList<T> : IList<T>
+{
+    private readonly List<T> list = new List<T>();
+    private readonly string keyName;
+
+    public SecurePlayerPrefsList(string keyName)
+    {
+        this.keyName = keyName;
+    }
+
+    public T this[int index] { get { return default(T); } set { } }
+
+    public int Count => 0;
+
+    public bool IsReadOnly => false;
+
+    public void Add(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int IndexOf(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Insert(int index, T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Utils/Unity/TransformUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class TransformUtils
+{
+
+    public static List<Transform> GetChildrenTransformList(this Transform transform)
+    {
+        List<Transform> children = new List<Transform>();
+
+        foreach (Transform child in transform)
+            children.Add(child);
+
+        return children;
+    }
+}
