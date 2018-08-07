0693bcaefa623cc361fc3e7354dc2129ac1fdb5c
Matthew LeLiever <matthew121250@hotmail.com>
2018-08-07 19:26:22 -0400

    Implementing new PlayerPref randomizer with the PrefSettingsRandomizer

---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Editor Only Scripts/RandomizeTextAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+/// <summary>
+/// Attribute to apply to fields that need to be changed once the build is created.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+public class RandomizeTextAttribute : Attribute
+{
+}
