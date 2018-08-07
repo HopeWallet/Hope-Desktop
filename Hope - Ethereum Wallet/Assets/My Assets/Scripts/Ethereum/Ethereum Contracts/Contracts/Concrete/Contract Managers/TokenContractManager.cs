43cf228ddb3787b0980e875cc2fba5f0d58ed897
Matthew LeLiever <matthew121250@hotmail.com>
2018-08-07 14:32:35 -0400

    Removed code and some comments

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/Ethereum/Ethereum Contracts/Contracts/Concrete/Contract Managers/TokenContractManager.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Ethereum/Ethereum Contracts/Contracts/Concrete/Contract Managers/TokenContractManager.cs
@@ -16,14 +16,7 @@ public class TokenContractManager
     private readonly TradableAssetImageManager tradableAssetImageManager;
     private readonly UserWalletManager userWalletManager;
 
-    private readonly Queue<string> tokensToInitialize = new Queue<string>();
-    private readonly Dictionary<string, int> addressButtonIndices = new Dictionary<string, int>();
-
     private readonly SecurePlayerPrefList<TokenInfoJson> tokens;
-
-    private int tokenPrefIndex,
-                savedTokenCount,
-                counter;
 
     /// <summary>
     /// Initializes the TokenContractManager by creating all collections and getting the settings.
@@ -43,7 +36,6 @@ public class TokenContractManager
         this.userWalletManager = userWalletManager;
 
         tokens = new SecurePlayerPrefList<TokenInfoJson>(settings.tokenPrefName);
-        //AddSavedTokensToQueue();
     }
 
     /// <summary>
@@ -53,10 +45,10 @@ public class TokenContractManager
     /// <returns> The index of the token. </returns>
     public int GetTokenIndex(string tokenAddress)
     {
-        if (!addressButtonIndices.ContainsKey(tokenAddress))
+        if (!tokens.Contains(tokenAddress))
             return 0;
 
-        return addressButtonIndices[tokenAddress];
+        return tokens.IndexOf(tokenAddress);
     }
 
     /// <summary>
@@ -66,23 +58,6 @@ public class TokenContractManager
     /// <param name="ignorePopup"> Whether the loading popup should be ignored. </param>
     public void AddToken(string tokenAddress)
     {
-        //var tokenPref = tokenAddress.ToLower();
-
-        //if (SecurePlayerPrefs.HasKey(tokenPref))
-        //    return;
-
-        //if (!ignorePopup)
-        //    popupManager.GetPopup<LoadingPopup>();
-
-        //InitializeToken(tokenPref, null, (abi, asset) =>
-        //{
-        //    UpdateTradableAssets(asset, () =>
-        //    {
-        //        SecurePlayerPrefs.SetString(settings.tokenPrefName + (tokenPrefIndex++), tokenPref);
-        //        SecurePlayerPrefs.SetString(tokenPref, abi);
-        //        popupManager.CloseActivePopup();
-        //    });
-        //});
         AddToken(tokenAddress, true);
     }
 
@@ -118,37 +93,6 @@ public class TokenContractManager
     /// <param name="tokenAddress"> The contract address of the token to remove. </param>
     public void RemoveToken(string tokenAddress)
     {
-        //bool startRemoving = false;
-
-        //for (int i = 0; ; i++)
-        //{
-        //    string prefName = settings.tokenPrefName + i;
-        //    string nextPrefName = settings.tokenPrefName + (i + 1);
-
-        //    if (!SecurePlayerPrefs.HasKey(prefName))
-        //        break;
-
-        //    string tokenAddress = SecurePlayerPrefs.GetString(prefName);
-
-        //    if (tokenAddress.EqualsIgnoreCase(addressToRemove))
-        //    {
-        //        tokenPrefIndex--;
-        //        startRemoving = true;
-
-        //        SecurePlayerPrefs.DeleteKey(tokenAddress);
-        //        OnTokenRemoved?.Invoke(addressToRemove);
-        //    }
-
-        //    if (startRemoving)
-        //    {
-        //        SecurePlayerPrefs.DeleteKey(prefName);
-
-        //        if (!SecurePlayerPrefs.HasKey(nextPrefName))
-        //            break;
-
-        //        SecurePlayerPrefs.SetString(prefName, SecurePlayerPrefs.GetString(nextPrefName));
-        //    }
-        //}
         tokens.Remove(tokenAddress);
         OnTokenRemoved?.Invoke(tokenAddress);
     }
@@ -188,70 +132,6 @@ public class TokenContractManager
 
         return false;
     }
-
-    /// <summary>
-    /// Adds the tokens saved in the PlayerPrefs to the queue.
-    /// Will be fully loaded later.
-    /// </summary>
-    //private void AddSavedTokensToQueue()
-    //{
-    //    for (int i = 0; ; i++)
-    //    {
-    //        string prefName = settings.tokenPrefName + i;
-
-    //        if (!SecurePlayerPrefs.HasKey(prefName))
-    //        {
-    //            tokenPrefIndex = i;
-    //            break;
-    //        }
-
-    //        string addressPref = SecurePlayerPrefs.GetString(prefName);
-
-    //        tokensToInitialize.Enqueue(addressPref);
-    //        addressButtonIndices.Add(addressPref, i + 1);
-    //    }
-
-    //    savedTokenCount = tokensToInitialize.Count + 1;
-    //}
-
-    /// <summary>
-    /// Loads the tokens from the TokensToInitialize queue recursively, until there are no more tokens to be loaded.
-    /// </summary>
-    /// <param name="onLoadingFinished"> Action to call once the token loading has completed. </param>
-    //private void LoadTokensFromQueue(Action onLoadingFinished = null)
-    //{
-    //    if (tokensToInitialize.Count == 0)
-    //        return;
-
-    //    string tokenAddress = tokensToInitialize.Dequeue();
-    //    string tokenAbi = SecurePlayerPrefs.GetString(tokenAddress);
-
-    //    InitializeToken(tokenAddress, tokenAbi, (_, asset) => UpdateTradableAssets(asset, () => CheckLoadStatus(onLoadingFinished)));
-    //    LoadTokensFromQueue(onLoadingFinished);
-    //}
-
-    /// <summary>
-    /// Checks the load status of the tokens, and if it is finished, executes the action and the event.
-    /// </summary>
-    /// <param name="onLoadFinished"> The action to execute once the load has finished. </param>
-    //private void CheckLoadStatus(Action onLoadFinished)
-    //{
-    //    if (++counter < savedTokenCount)
-    //        return;
-
-    //    OnTokensLoaded?.Invoke();
-    //    onLoadFinished?.Invoke();
-    //}
-
-    /// <summary>
-    /// Initializes a TokenContract and TokenAsset given a contract address and abi.
-    /// </summary>
-    /// <param name="contractAddress"> The address of the token contract. </param>
-    /// <param name="contractAbi"> The abi of the contract. </param>
-    /// <param name="onTokenAssetCreated"> Action called once the TokenContract and TokenAsset have finished being created. String param is abi, TradableAsset param is the TokenAsset. </param>
-    //private void InitializeToken(string contractAddress, string contractAbi, Action<string, TradableAsset> onTokenAssetCreated)
-    //    => new TokenContract(contractAddress, contractAbi, (contract, abi)
-    //        => new ERC20TokenAsset(contract as TokenContract, asset => onTokenAssetCreated?.Invoke(abi, asset), tradableAssetImageManager, userWalletManager));
 
     /// <summary>
     /// Adds the newly created TradableAsset to the TradableAssetManager and TradableAssetButtonManager.
