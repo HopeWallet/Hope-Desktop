60cc1869878d17be491d6bf134ecb4d9617c4793
Matthew LeLiever <matthew121250@hotmail.com>
2018-08-08 17:17:43 -0400

    Updates

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/Wallet/General/UserWalletInfoManager.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Wallet/General/UserWalletInfoManager.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public sealed class UserWalletInfoManager
+﻿public sealed class UserWalletInfoManager
 {
     private readonly SecurePlayerPrefList<WalletInfo> wallets;
     private readonly UserWalletManager.Settings walletSettings;
