77d069d676c4af94acc47e22068f05960325cf41
Matthew LeLiever <matthew121250@hotmail.com>
2018-08-10 14:49:17 -0400

    Encryption updates

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/Security/Encryption/Concrete Implementations/EternalEncryption.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Security/Encryption/Concrete Implementations/EternalEncryption.cs
@@ -1,18 +1,42 @@
-﻿using System;
+﻿using Hope.Security.Encryption.Symmetric;
+using System;
 
 /// <summary>
 /// Class which encrypts data which can be decrypted over several sessions of lifetime.
 /// </summary>
-public sealed class EternalEncryption
+public static class EternalEncryption
 {
-    private readonly bool isWindows;
+    private static bool IsWindows = Environment.OSVersion.Platform == PlatformID.Win32NT;
 
-    /// <summary>
-    /// Initializes the <see cref="EternalEncryption"/> object.
-    /// </summary>
-    public EternalEncryption()
+    public static string Encrypt(string data, string entropy)
     {
-        isWindows = Environment.OSVersion.Platform == PlatformID.Win32NT;
     }
 
+    public static string Encrypt(string data, byte[] entropy)
+    {
+    }
+
+    public static byte[] Encrypt(byte[] data, string entropy)
+    {
+    }
+
+    public static byte[] Encrypt(byte[] data, byte[] entropy)
+    {
+    }
+
+    public static string Decrypt(string data, string entropy)
+    {
+    }
+
+    public static string Decrypt(string data, byte[] entropy)
+    {
+    }
+
+    public static byte[] Decrypt(byte[] data, string entropy)
+    {
+    }
+
+    public static byte[] Decrypt(byte[] data, byte[] entropy)
+    {
+    }
 }
--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/Security/Encryption/Symmetric Encryption/Abstract/SymmetricEncryptor.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Security/Encryption/Symmetric Encryption/Abstract/SymmetricEncryptor.cs
@@ -37,6 +37,49 @@ namespace Hope.Security.Encryption.Symmetric
         public static string Encrypt(string text, string entropy) => Encrypt(text, entropy, ITERATIONS);
 
         /// <summary>
+        /// Encrypts <see langword="string"/> text using the <see cref="SymmetricAlgorithm"/> of the derived class.
+        /// </summary>
+        /// <param name="text"> The <see langword="string"/> text to encrypt. </param>
+        /// <param name="entropy"> The additional entropy to apply to the encryption. </param>
+        /// <param name="iterations"> The number of iterations to apply to the encryption. </param>
+        /// <returns> The encrypted <see langword="string"/> text. </returns>
+        public static string Encrypt(string text, string entropy, int iterations) => Encrypt(text.GetUTF8Bytes(), entropy, iterations).GetBase64String();
+
+        /// <summary>
+        /// Encrypts <see langword="byte"/>[] data using the <see cref="SymmetricAlgorithm"/> of the derived class.
+        /// </summary>
+        /// <param name="data"> The <see langword="byte"/>[] data to encrypt. </param>
+        /// <param name="entropy"> The additional entropy to apply to the encryption. </param>
+        /// <returns> The encrypted <see langword="byte"/>[] data. </returns>
+        public static byte[] Encrypt(byte[] data, string entropy) => Encrypt(data, entropy, ITERATIONS);
+
+        /// <summary>
+        /// Encrypts <see langword="byte"/>[] data using the <see cref="SymmetricAlgorithm"/> of the derived class.
+        /// </summary>
+        /// <param name="data"> The <see langword="byte"/>[] data to encrypt. </param>
+        /// <param name="entropy"> The additional entropy to apply to the encryption. </param>
+        /// <param name="iterations"> The number of iterations to apply to the encryption. </param>
+        /// <returns> The encrypted <see langword="byte"/>[] data. </returns>
+        public static byte[] Encrypt(byte[] data, string entropy, int iterations) => Encrypt(data, entropy.GetUTF8Bytes(), iterations);
+
+        /// <summary>
+        /// Encrypts <see langword="byte"/>[] data using the <see cref="SymmetricAlgorithm"/> of the derived class.
+        /// </summary>
+        /// <param name="data"> The <see langword="byte"/>[] data to encrypt. </param>
+        /// <param name="entropy"> The additional entropy to apply to the encryption. </param>
+        /// <returns> The encrypted <see langword="byte"/>[] data. </returns>
+        public static byte[] Encrypt(byte[] data, byte[] entropy) => Encrypt(data, entropy, ITERATIONS);
+
+        /// <summary>
+        /// Encrypts <see langword="byte"/>[] data using the <see cref="SymmetricAlgorithm"/> of the derived class.
+        /// </summary>
+        /// <param name="data"> The <see langword="byte"/>[] data to encrypt. </param>
+        /// <param name="entropy"> The additional entropy to apply to the encryption. </param>
+        /// <param name="iterations"> The number of iterations to apply to the encryption. </param>
+        /// <returns> The encrypted <see langword="byte"/>[] data. </returns>
+        public static byte[] Encrypt(byte[] data, byte[] entropy, int iterations) => Encryptor.InternalEncrypt(data, entropy, iterations);
+
+        /// <summary>
         /// Decrypts encrypted <see langword="string"/> text using the <see cref="SymmetricAlgorithm"/> of the derived class.
         /// </summary>
         /// <param name="encryptedText"> The encrypted <see langword="string"/> text. </param>
@@ -45,30 +88,13 @@ namespace Hope.Security.Encryption.Symmetric
         public static string Decrypt(string encryptedText, string entropy) => Decrypt(encryptedText, entropy, ITERATIONS);
 
         /// <summary>
-        /// Encrypts <see langword="string"/> text using the <see cref="SymmetricAlgorithm"/> of the derived class.
-        /// </summary>
-        /// <param name="text"> The <see langword="string"/> text to encrypt. </param>
-        /// <param name="entropy"> The additional entropy to apply to the encryption. </param>
-        /// <param name="iterations"> The number of iterations to apply to the encryption. </param>
-        /// <returns> The encrypted <see langword="string"/> text. </returns>
-        public static string Encrypt(string text, string entropy, int iterations) => Encryptor.InternalEncrypt(text.GetUTF8Bytes(), entropy, iterations).GetBase64String();
-
-        /// <summary>
         /// Decrypts encrypted <see langword="string"/> text using the <see cref="SymmetricAlgorithm"/> of the derived class.
         /// </summary>
         /// <param name="encryptedText"> The encrypted <see langword="string"/> text. </param>
         /// <param name="entropy"> The additional entropy to apply to the decryption. </param>
         /// <param name="iterations"> The number of iterations to apply to the decryption. </param>
         /// <returns> The decrypted <see langword="string"/> text. </returns>
-        public static string Decrypt(string encryptedText, string entropy, int iterations) => Encryptor.InternalDecrypt(encryptedText.GetBase64Bytes(), entropy, iterations).GetUTF8String();
-
-        /// <summary>
-        /// Encrypts <see langword="byte"/>[] data using the <see cref="SymmetricAlgorithm"/> of the derived class.
-        /// </summary>
-        /// <param name="data"> The <see langword="byte"/>[] data to encrypt. </param>
-        /// <param name="entropy"> The additional entropy to apply to the encryption. </param>
-        /// <returns> The encrypted <see langword="byte"/>[] data. </returns>
-        public static byte[] Encrypt(byte[] data, string entropy) => Encryptor.InternalEncrypt(data, entropy, ITERATIONS);
+        public static string Decrypt(string encryptedText, string entropy, int iterations) => Decrypt(encryptedText.GetBase64Bytes(), entropy, iterations).GetUTF8String();
 
         /// <summary>
         /// Decrypts encrypted <see langword="byte"/>[] data.
@@ -76,16 +102,7 @@ namespace Hope.Security.Encryption.Symmetric
         /// <param name="encryptedData"> The encrypted <see langword="byte"/>[] data. </param>
         /// <param name="entropy"> The additional entropy to apply to the decryption. </param>
         /// <returns> The decrypted <see langword="byte"/>[] data. </returns>
-        public static byte[] Decrypt(byte[] encryptedData, string entropy) => Encryptor.InternalDecrypt(encryptedData, entropy, ITERATIONS);
-
-        /// <summary>
-        /// Encrypts <see langword="byte"/>[] data using the <see cref="SymmetricAlgorithm"/> of the derived class.
-        /// </summary>
-        /// <param name="data"> The <see langword="byte"/>[] data to encrypt. </param>
-        /// <param name="entropy"> The additional entropy to apply to the encryption. </param>
-        /// <param name="iterations"> The number of iterations to apply to the encryption. </param>
-        /// <returns> The encrypted <see langword="byte"/>[] data. </returns>
-        public static byte[] Encrypt(byte[] data, string entropy, int iterations) => Encryptor.InternalEncrypt(data, entropy, iterations);
+        public static byte[] Decrypt(byte[] encryptedData, string entropy) => Decrypt(encryptedData, entropy, ITERATIONS);
 
         /// <summary>
         /// Decrypts encrypted <see langword="byte"/>[] data.
@@ -94,7 +111,24 @@ namespace Hope.Security.Encryption.Symmetric
         /// <param name="entropy"> The additional entropy to apply to the decryption. </param>
         /// <param name="iterations"> The number of iterations to apply to the decryption. </param>
         /// <returns> The decrypted <see langword="byte"/>[] data. </returns>
-        public static byte[] Decrypt(byte[] encryptedData, string entropy, int iterations) => Encryptor.InternalDecrypt(encryptedData, entropy, iterations);
+        public static byte[] Decrypt(byte[] encryptedData, string entropy, int iterations) => Decrypt(encryptedData, entropy.GetUTF8Bytes(), iterations);
+
+        /// <summary>
+        /// Decrypts encrypted <see langword="byte"/>[] data.
+        /// </summary>
+        /// <param name="encryptedData"> The encrypted <see langword="byte"/>[] data. </param>
+        /// <param name="entropy"> The additional entropy to apply to the decryption. </param>
+        /// <returns> The decrypted <see langword="byte"/>[] data. </returns>
+        public static byte[] Decrypt(byte[] encryptedData, byte[] entropy) => Decrypt(encryptedData, entropy, ITERATIONS);
+
+        /// <summary>
+        /// Decrypts encrypted <see langword="byte"/>[] data.
+        /// </summary>
+        /// <param name="encryptedData"> The encrypted <see langword="byte"/>[] data. </param>
+        /// <param name="entropy"> The additional entropy to apply to the decryption. </param>
+        /// <param name="iterations"> The number of iterations to apply to the decryption. </param>
+        /// <returns> The decrypted <see langword="byte"/>[] data. </returns>
+        public static byte[] Decrypt(byte[] encryptedData, byte[] entropy, int iterations) => Encryptor.InternalDecrypt(encryptedData, entropy, iterations);
 
         /// <summary>
         /// Encrypts <see langword="byte"/>[] data using the <see cref="SymmetricAlgorithm"/> provided.
@@ -103,7 +137,7 @@ namespace Hope.Security.Encryption.Symmetric
         /// <param name="entropy"> The entropy to use to encrypt the data. </param>
         /// <param name="iterations"> The number of iterations to apply to <see cref="Rfc2898DeriveBytes"/> to derive the encryption key. </param>
         /// <returns> The encrypted <see langword="byte"/>[] data. </returns>
-        private byte[] InternalEncrypt(byte[] data, string entropy, int iterations)
+        private byte[] InternalEncrypt(byte[] data, byte[] entropy, int iterations)
         {
             byte[] saltStringBytes = SecureRandom.GetNextBytes(Random, SaltIvByteSize);
             byte[] ivStringBytes = SecureRandom.GetNextBytes(Random, SaltIvByteSize);
@@ -136,7 +170,7 @@ namespace Hope.Security.Encryption.Symmetric
         /// <param name="entropy"> The entropy to use to decrypt the data. </param>
         /// <param name="iterations"> The number of iterations to apply to <see cref="Rfc2898DeriveBytes"/> to derive the decryption key. </param>
         /// <returns> The decrypted <see langword="byte"/>[] data. </returns>
-        private byte[] InternalDecrypt(byte[] encryptedData, string entropy, int iterations)
+        private byte[] InternalDecrypt(byte[] encryptedData, byte[] entropy, int iterations)
         {
             byte[] saltStringBytes = encryptedData.Take(KeySize / 8).ToArray();
             byte[] ivStringBytes = encryptedData.Skip(KeySize / 8).Take(KeySize / 8).ToArray();
