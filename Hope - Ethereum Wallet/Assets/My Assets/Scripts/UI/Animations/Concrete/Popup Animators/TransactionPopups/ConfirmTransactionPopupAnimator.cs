b9ebf2c7746b51aaa177a9e40f21c6af85eb05a6
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-08-13 00:58:37 -0400

    More updates and animations that have been redone

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Animations/Concrete/Popup Animators/TransactionPopups/ConfirmTransactionPopupAnimator.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Animations/Concrete/Popup Animators/TransactionPopups/ConfirmTransactionPopupAnimator.cs
@@ -6,16 +6,37 @@ public sealed class ConfirmTransactionPopupAnimator : CountdownTimerAnimator
 	[SerializeField] private GameObject tokenIcon;
 	[SerializeField] private GameObject fromAddressSection;
 	[SerializeField] private GameObject toAddressSection;
+	[SerializeField] private GameObject line;
 	[SerializeField] private GameObject transactionSection;
 	[SerializeField] private GameObject feeSection;
 
+	/// <summary>
+	/// Animates the unique elements of this form into view
+	/// </summary>
 	protected override void AnimateUniqueElementsIn()
 	{
-		throw new System.NotImplementedException();
+		tokenIcon.AnimateGraphicAndScale(1f, 1f, 0.15f);
+		fromAddressSection.AnimateScaleX(1f, 0.18f);
+		toAddressSection.AnimateScaleX(1f, 0.22f);
+		line.AnimateScaleX(1f, 0.235f);
+		transactionSection.AnimateScaleX(1f, 0.25f);
+		feeSection.AnimateScaleX(1f, 0.27f, StartTimerAnimation);
+		confirmButton.AnimateGraphicAndScale(1f, 1f, 0.3f);
+		cancelButton.AnimateGraphicAndScale(1f, 1f, 0.3f, FinishedAnimating);
 	}
 
+	/// <summary>
+	/// Animates the unique elements of this form out of view
+	/// </summary>
 	protected override void AnimateUniqueElementsOut()
 	{
-		throw new System.NotImplementedException();
+		cancelButton.AnimateGraphicAndScale(0f, 0f, 0.15f);
+		confirmButton.AnimateGraphicAndScale(0f, 0f, 0.15f);
+		feeSection.AnimateScaleX(0f, 0.18f);
+		transactionSection.AnimateScaleX(0f, 0.22f, () => AnimateBasicElements(false));
+		line.AnimateScaleX(0f, 0.235f);
+		fromAddressSection.AnimateScaleX(0f, 0.25f);
+		toAddressSection.AnimateScaleX(0f, 0.27f);
+		tokenIcon.AnimateGraphicAndScale(0f, 0f, 0.3f);
 	}
 }
