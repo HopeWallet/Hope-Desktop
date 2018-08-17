0479a63f01a736756e3fbd3c8de7a60520d77561
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-08-17 19:27:12 -0400

    More updates to the new input fields

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/HopeInputField.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/HopeInputField.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using RandomNET.Strings;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,22 +12,10 @@ public class HopeInputField : MonoBehaviour
 
 	public TextMeshProUGUI ErrorMessage;
 
-	private Sprite eyeInactive;
-	private Sprite eyeActive;
+	private Sprite eyeInactiveNormal;
+	private Sprite eyeActiveNormal;
 
-	private bool error;
-
-	public bool Error
-	{
-		get { return error; }
-		set
-		{
-			error = value;
-
-			errorIcon.AnimateGraphic(error ? 1f : 0f, 0.1f);
-			ErrorMessage.gameObject.AnimateGraphic(error ? 1f : 0f, 0.1f);
-		}
-	}
+	public bool Error { get; set; }
 
 	private void Awake()
 	{
@@ -36,26 +25,28 @@ public class HopeInputField : MonoBehaviour
 		{
 			eye.GetComponent<Button>().onClick.AddListener(EyeClicked);
 
-			var inactive = Resources.Load("UI/Graphics/Textures/New/Icons/Eye_Inactive") as Texture2D;
-			var active = Resources.Load("UI/Graphics/Textures/New/Icons/Eye_Active") as Texture2D;
-
-			eyeInactive = Sprite.Create(inactive, new Rect(0f, 0f, inactive.width, inactive.height), new Vector2(0.5f, 0.5f));
-			eyeActive = Sprite.Create(active, new Rect(0f, 0f, active.width, active.height), new Vector2(0.5f, 0.5f));
+			SetSprite(ref eyeInactiveNormal, "Eye_Inactive_Normal");
+			SetSprite(ref eyeActiveNormal, "Eye_Active_Normal");
 		}
+	}
+
+	private void SetSprite(ref Sprite targetSprite, string iconName)
+	{
+		Texture2D loadedTexture = Resources.Load("UI/Graphics/Textures/New/Icons/" + iconName) as Texture2D;
+		targetSprite = Sprite.Create(loadedTexture, new Rect(0f, 0f, loadedTexture.width, loadedTexture.height), new Vector2(0.5f, 0.5f));
 	}
 
 	private void InputFieldChanged(string str)
 	{
 		bool emptyString = string.IsNullOrEmpty(str);
 
-		if (!error)
-		{
-			placeholder.AnimateTransformY(emptyString ? 0f : 35f, 0.1f);
-			inputFieldBase.gameObject.AnimateColor(emptyString ? new Color(0.85f, 0.85f, 0.85f) : UIColors.Green, 0.1f);
-		}
+		placeholder.AnimateTransformY(emptyString ? 0f : 35f, 0.1f);
+		inputFieldBase.gameObject.AnimateColor(emptyString ? new Color(0.85f, 0.85f, 0.85f) : Error ? UIColors.Red : UIColors.Green, 0.1f);
+		errorIcon.AnimateGraphic(emptyString ? 0f : Error ? 1f : 0f, 0.1f);
+		ErrorMessage.gameObject.AnimateGraphic(emptyString ? 0f : Error ? 1f : 0f, 0.1f);
 
 		if (eye != null)
-			eye.SetActive(!emptyString);
+			eye.AnimateGraphicAndScale(emptyString ? 0f : 1f, emptyString ? 0f : 1f, 0.1f);
 	}
 
 	private void EyeClicked()
@@ -72,6 +63,6 @@ public class HopeInputField : MonoBehaviour
 			inputFieldBase.text.ForEach(_ => inputFieldBase.textComponent.text += "*");
 		}
 
-		eye.GetComponent<Image>().sprite = inputFieldBase.contentType == InputField.ContentType.Password ? eyeInactive : eyeActive;
+		eye.GetComponent<Image>().sprite = inputFieldBase.contentType == InputField.ContentType.Password ? eyeInactiveNormal : eyeActiveNormal;
 	}
 }
--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Other/CursorChanger.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Other/CursorChanger.cs
@@ -3,22 +3,34 @@ using UnityEngine.EventSystems;
 
 public class CursorChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-	private Texture2D textCursor;
+	public bool hand;
+
+	private Texture2D textCursor, handCursor;
 
 	/// <summary>
 	/// Loads the text cursor from the resources folder
 	/// </summary>
-	void Awake () => textCursor = Resources.Load("UI/Graphics/Textures/New/Icons/TextCursor_Icon") as Texture2D;
+	void Awake()
+	{
+		textCursor = Resources.Load("UI/Graphics/Textures/New/Icons/TextCursor_Icon") as Texture2D;
+		handCursor = Resources.Load("UI/Graphics/Textures/New/Icons/HandCursor_Icon") as Texture2D;
+	}
 
 	/// <summary>
 	/// Sets cursor image to the text cursor icon
 	/// </summary>
 	/// <param name="eventData"> The PointerEventData </param>
-	public void OnPointerEnter(PointerEventData eventData) => Cursor.SetCursor(textCursor, new Vector2(45f, 25f), CursorMode.Auto);
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		Cursor.SetCursor(hand ? handCursor : textCursor, new Vector2(hand ? 12f : 45f, hand ? 0f : 25f), CursorMode.Auto);
+	}
 
 	/// <summary>
 	/// Sets cursor image back to the default
 	/// </summary>
 	/// <param name="eventData"> The PointerEventData </param>
-	public void OnPointerExit(PointerEventData eventData) => Cursor.SetCursor(null, new Vector2(45f, 25f), CursorMode.Auto);
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		Cursor.SetCursor(null, new Vector2(hand ? 12f : 45f, hand ? 0f : 25f), CursorMode.Auto);
+	}
 }
