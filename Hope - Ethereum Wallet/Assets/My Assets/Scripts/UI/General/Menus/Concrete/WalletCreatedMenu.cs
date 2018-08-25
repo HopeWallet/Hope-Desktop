30ff390f762565d77e73bd49f6c4a4c25b773f02
Matthew LeLiever <matthew121250@hotmail.com>
2018-08-24 23:04:28 -0400

    Fixes
    Co-Authored-By: Mitchell <abstractvoid@users.noreply.github.com>

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Menus/Concrete/WalletCreatedMenu.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Menus/Concrete/WalletCreatedMenu.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Class used for displaying a menu which shows the wallet was created.
+/// </summary>
 public class WalletCreatedMenu : Menu<WalletCreatedMenu>
 {
     [SerializeField] private Button openWalletButton;
 
+    /// <summary>
+    /// Adds the listener to the openWalletButton which opens the OpenWalletMenu.
+    /// </summary>
     private void Start() => openWalletButton.onClick.AddListener(() => uiManager.OpenMenu<OpenWalletMenu>());
 }
