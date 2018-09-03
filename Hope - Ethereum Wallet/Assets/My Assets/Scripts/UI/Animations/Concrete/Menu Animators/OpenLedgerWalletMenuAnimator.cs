2ea8eb3b247d948041d85212399d680f2b6914a9
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-09-03 18:36:24 -0400

    Created OpenTrezorWallet_Menu

---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Animations/Concrete/Menu Animators/OpenLedgerWalletMenuAnimator.cs
@@ -0,0 +1,11 @@
+﻿
+public class OpenLedgerWalletMenuAnimator : UIAnimator
+{
+	/// <summary>
+	/// Animates the unique elements into view
+	/// </summary>
+	protected override void AnimateUniqueElementsIn()
+	{
+		FinishedAnimating();
+	}
+}
---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Animations/Concrete/Menu Animators/OpenTrezorWalletMenuAnimator.cs
@@ -0,0 +1,11 @@
+﻿
+public class OpenTrezorWalletMenuAnimator : UIAnimator {
+
+	/// <summary>
+	/// Animates the unique elements into view
+	/// </summary>
+	protected override void AnimateUniqueElementsIn()
+	{
+		FinishedAnimating();
+	}
+}
