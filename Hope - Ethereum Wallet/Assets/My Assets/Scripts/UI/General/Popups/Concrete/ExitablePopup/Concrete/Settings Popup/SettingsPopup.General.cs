54b218e2d3e549c0eb503b9cd3646754c23751e0
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-09-06 10:38:14 -0400

    Fixes to the SettingsPopup partial classes

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Popups/Concrete/ExitablePopup/Concrete/Settings Popup/SettingsPopup.General.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Popups/Concrete/ExitablePopup/Concrete/Settings Popup/SettingsPopup.General.cs
@@ -1,9 +1,26 @@
 ﻿using UnityEngine;
 
-public sealed partial class SettingsPopup : MonoBehaviour
+public sealed partial class SettingsPopup : ExitablePopupComponent<SettingsPopup>
 {
 	public sealed class General
 	{
+		private GameObject defaultCurrencyDropdown;
+		private CheckBox idleTimeoutTimeCheckbox, countdownTimerCheckbox, transactionNotificationCheckbox, updateNotificationCheckbox;
+		private HopeInputField idleTimeoutTimeInputField;
 
+		public General(GameObject defaultCurrencyDropdown,
+					   CheckBox idleTimeoutTimeCheckbox,
+					   CheckBox countdownTimerCheckbox,
+					   CheckBox transactionNotificationCheckbox,
+					   CheckBox updateNotificationCheckbox,
+					   HopeInputField idleTimeoutTimeInputField)
+		{
+			this.defaultCurrencyDropdown = defaultCurrencyDropdown;
+			this.idleTimeoutTimeCheckbox = idleTimeoutTimeCheckbox;
+			this.countdownTimerCheckbox = countdownTimerCheckbox;
+			this.transactionNotificationCheckbox = transactionNotificationCheckbox;
+			this.updateNotificationCheckbox = updateNotificationCheckbox;
+			this.idleTimeoutTimeInputField = idleTimeoutTimeInputField;
+		}
 	}
 }
--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Popups/Concrete/ExitablePopup/Concrete/Settings Popup/SettingsPopup.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Popups/Concrete/ExitablePopup/Concrete/Settings Popup/SettingsPopup.cs
@@ -3,11 +3,19 @@ using UnityEngine.UI;
 
 public sealed partial class SettingsPopup : ExitablePopupComponent<SettingsPopup>
 {
+	[SerializeField] private GameObject defaultCurrencyDropdown;
+	[SerializeField] private CheckBox idleTimeoutTimeCheckbox, countdownTimerCheckbox, transactionNotificationCheckbox, updateNotificationCheckbox;
+	[SerializeField] private HopeInputField idleTimeoutTimeInputField;
+
 	[SerializeField] private Button downloadUpdateButton;
+
+	private General general;
 
 	protected override void Awake()
 	{
 		base.Awake();
+
+		general = new General(defaultCurrencyDropdown, idleTimeoutTimeCheckbox, countdownTimerCheckbox, transactionNotificationCheckbox, updateNotificationCheckbox, idleTimeoutTimeInputField);
 
 		downloadUpdateButton.onClick.AddListener(() => Application.OpenURL("http://www.hopewallet.io/"));
 	}
