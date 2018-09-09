29bd172e518f85761a9dce13f381e70f2c563cc9
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-09-08 23:23:04 -0400

    Additions to OpenLedgerWallet_Menu

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Animations/Concrete/Menu Animators/OpenLedgerWalletMenuAnimator.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Animations/Concrete/Menu Animators/OpenLedgerWalletMenuAnimator.cs
@@ -1,6 +1,23 @@
 ﻿
+using TMPro;
+using UnityEngine;
+
 public class OpenLedgerWalletMenuAnimator : UIAnimator
 {
+	[SerializeField] private GameObject awaitingConnectionText;
+	[SerializeField] private GameObject loadingIcon;
+	[SerializeField] private GameObject openWalletButton;
+
+	private OpenLedgerWalletMenu openLedgerWalletMenu;
+
+	private void Awake()
+	{
+		openLedgerWalletMenu.transform.GetComponent<OpenLedgerWalletMenu>();
+
+		openLedgerWalletMenu.OnLedgerConnected += () => ChangeLedgerStatus(true);
+		openLedgerWalletMenu.OnLedgerDisconnected += () => ChangeLedgerStatus(false);
+	}
+
 	/// <summary>
 	/// Animates the unique elements into view
 	/// </summary>
@@ -16,4 +33,22 @@ public class OpenLedgerWalletMenuAnimator : UIAnimator
 	{
 		FinishedAnimating();
 	}
+
+	private void ChangeLedgerStatus(bool ledgerConnected)
+	{
+		awaitingConnectionText.GetComponent<LoadingTextAnimator>().enabled = !ledgerConnected;
+
+		awaitingConnectionText.AnimateGraphicAndScale(0f, 0f, 0.15f, () =>
+		{
+			awaitingConnectionText.GetComponent<TextMeshProUGUI>().text = ledgerConnected ? "Device found" : "Awaiting connection";
+			awaitingConnectionText.AnimateGraphicAndScale(1f, 1f, 0.15f);
+		});
+
+		if (ledgerConnected)
+			loadingIcon.AnimateGraphicAndScale(0f, 0f, 0.15f, () => openWalletButton.AnimateGraphicAndScale(1f, 1f, 0.15f));
+		else
+			openWalletButton.AnimateGraphicAndScale(0f, 0f, 0.15f, () => loadingIcon.AnimateGraphicAndScale(1f, 1f, 0.15f));
+	}
+
+
 }
