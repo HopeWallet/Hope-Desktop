3e140e68fc1babed89ab3c5e64cd8b45511c3b93
Matthew LeLiever <matthew121250@hotmail.com>
2018-09-08 23:15:35 -0400

    Started OpenLedgerWalletMenu implementation

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Menus/Concrete/OpenLedgerWalletMenu.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Menus/Concrete/OpenLedgerWalletMenu.cs
@@ -1,7 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenLedgerWalletMenu : Menu<OpenLedgerWalletMenu>
+public class OpenLedgerWalletMenu : WalletLoadMenuBase<OpenLedgerWalletMenu>
 {
+    public event Action OnLedgerConnected;
+    public event Action OnLedgerDisconnected;
+    public override event Action OnWalletLoading;
+
+    public override void LoadWallet()
+    {
+    }
 }
