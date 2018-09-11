94e29d647ba0e856f72457ff6872079fc68bcb2a
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-09-11 16:04:47 -0400

    Accounts_Popup fixes and changes

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Popups/Concrete/OkCancelPopup/Concrete/AccountsPopup.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Popups/Concrete/OkCancelPopup/Concrete/AccountsPopup.cs
@@ -4,32 +4,83 @@ using UnityEngine.UI;
 
 public class AccountsPopup : OkCancelPopupComponent<AccountsPopup>
 {
-	[SerializeField] private GeneralRadioButtons addressesSection;
+	[SerializeField] private GeneralRadioButtons addressesCategories;
+	[SerializeField] private Transform addressesSection;
 	[SerializeField] private Button previousPageButton, nextPageButton;
 	[SerializeField] private TextMeshProUGUI pageNumText;
 
-	private int pageNum = 1, firstAddressNumInList = 1;
+	private int pageNum = 1, firstAddressNumInList = 1, currentlySelectedAddress = 1, currentlyActivatedAddress = 1;
 
 	protected override void Awake()
 	{
 		base.Awake();
 
+		addressesCategories.OnButtonChanged += AddressCategoryChanged;
 		previousPageButton.onClick.AddListener(() => PageChanged(false));
 		nextPageButton.onClick.AddListener(() => PageChanged(true));
+
+		for (int i = 0; i < 5; i++)
+			SetButtonListener(i);
+	}
+
+	private void SetButtonListener(int num) => addressesSection.GetChild(num).GetComponent<Button>().onClick.AddListener(() => AddressClicked(num));
+
+	private void AddressClicked(int num)
+	{
+		SetAddressInteractable(addressesSection.GetChild(currentlySelectedAddress % 5), true);
+
+		Transform selectedAddressTransform = addressesSection.GetChild(num);
+
+		SetAddressInteractable(selectedAddressTransform, false);
+		currentlySelectedAddress = int.Parse(selectedAddressTransform.GetChild(0).GetComponent<TextMeshProUGUI>().text);
+
+		okButton.interactable = currentlyActivatedAddress != currentlySelectedAddress;
 	}
 
 	private void OnDestroy() => MoreDropdown.PopupClosed?.Invoke();
 
-	private void PageChanged(bool nextPage)
+	private void PageChanged(bool movingForward)
 	{
-		pageNum = nextPage ? ++pageNum : --pageNum;
-		firstAddressNumInList = nextPage ? firstAddressNumInList += 5 : firstAddressNumInList -= 5;
+		if (pageNum == 1 && !movingForward)
+			pageNum = 10;
+		else if (pageNum == 10 && movingForward)
+			pageNum = 1;
+		else
+			pageNum = movingForward ? ++pageNum : --pageNum;
+
+		firstAddressNumInList = (pageNum * 5) - 4;
 		pageNumText.text = pageNum.ToString();
 
 		for (int i = 0; i < 5; i++)
-			addressesSection.transform.GetChild(i).GetChild(0).GetComponent<TextMeshProUGUI>().text = (firstAddressNumInList + i).ToString();
+		{
+			Transform addressTransform = addressesSection.GetChild(i);
 
-		nextPageButton.interactable = pageNum != 10;
-		previousPageButton.interactable = pageNum != 1;
+			addressTransform.GetChild(0).GetComponent<TextMeshProUGUI>().text = (firstAddressNumInList + i).ToString();
+
+			if (currentlySelectedAddress == (firstAddressNumInList + i))
+				SetAddressInteractable(addressTransform, false);
+			else
+				SetAddressInteractable(addressTransform, true);
+		}
+	}
+
+	private void AddressCategoryChanged(int num)
+	{
+		if (num == 0)
+		{
+			//Default addresses
+		}
+		else
+		{
+			//Ledger legacy addresses
+		}
+	}
+
+	private void SetAddressInteractable(Transform addressTransform, bool interactable)
+	{
+		addressTransform.GetComponent<Button>().interactable = interactable;
+
+		for (int i = 0; i < addressTransform.childCount; i++)
+			addressTransform.GetChild(i).gameObject.AnimateColor(interactable ? UIColors.LightGrey : UIColors.White, 0.15f);
 	}
 }
