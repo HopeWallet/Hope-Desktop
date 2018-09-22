0d6947470a3c1b84bc686ea4042306f603459086
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-09-22 12:51:15 -0400

    Changes and fixes

---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Other/Interactable Elements/InteractableGeneral.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class InteractableGeneral : InteractableBase
+{
+	private void Start()
+	{
+		customCursor = Resources.Load("UI/Graphics/Textures/Icons/HandCursor_Icon") as Texture2D;
+		customCursorPosition = new Vector2(12f, 5f);
+	}
+}
