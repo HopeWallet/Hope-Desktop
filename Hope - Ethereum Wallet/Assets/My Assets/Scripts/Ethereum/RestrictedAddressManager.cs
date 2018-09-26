2b11bec6e8fc2b64b9e4c90cd882ef7f6583d61a
Matthew LeLiever <matthew121250@hotmail.com>
2018-09-26 00:26:13 -0400

    Fixed RestrictedAddressManager

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/Ethereum/RestrictedAddressManager.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Ethereum/RestrictedAddressManager.cs
@@ -1,14 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using Hope.Utils.Ethereum;
+using System.Linq;
 
+/// <summary>
+/// Class which manages the restricted addresses which cannot have tokens/eth sent to and cannot have contacts created from.
+/// </summary>
 public class RestrictedAddressManager
 {
+    private readonly TokenListManager tokenListManager;
 
+    private readonly string[] constantRestrictedAddresses = new string[]
+    {
+        "0x0000000000000000000000000000000000000000"
+    };
+
+    /// <summary>
+    /// Initializes the RestrictedAddressManager.
+    /// </summary>
+    /// <param name="tokenListManager"> The active TokenListManager. </param>
+    public RestrictedAddressManager(TokenListManager tokenListManager)
+    {
+        this.tokenListManager = tokenListManager;
+    }
+
+    /// <summary>
+    /// Checks if a given address is restricted.
+    /// </summary>
+    /// <param name="address"> The address to check. </param>
+    /// <returns> True if the address is restricted and cannot be used. </returns>
     public bool IsRestrictedAddress(string address)
     {
-		//Make sure all addresses are converted: .ToLower(); before checking
-
-		return false;
+        UnityEngine.Debug.Log(address);
+        return !AddressUtils.IsValidEthereumAddress(address)
+            || constantRestrictedAddresses.ContainsIgnoreCase(address)
+            || tokenListManager.TokenList.Select(token => token.TokenInfo.Address).ContainsIgnoreCase(address);
     }
 }
