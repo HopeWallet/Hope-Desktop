79199b81a65d0396de6931daf62ececb96313021
Mitchell LeLiever <33007416+MitchLeLiever@users.noreply.github.com>
2018-09-30 22:51:38 -0400

    Created the ConfirmReleasePopup

---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/Animations/Concrete/Popup Animators/PRPSLockPopups/ConfirmReleasePopupAnimator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// The animator class of the ConfirmReleasePopup
+/// </summary>
+public sealed class ConfirmReleasePopupAnimator : PopupAnimator
+{
+	[SerializeField] private GameObject questionText;
+	[SerializeField] private GameObject line;
+	[SerializeField] private GameObject purposeNameText;
+	[SerializeField] private GameObject prpsIcon;
+	[SerializeField] private GameObject currentPRPSText;
+	[SerializeField] private GameObject additionPRPSText;
+	[SerializeField] private GameObject confirmButton;
+	[SerializeField] private GameObject cancelButton;
+	[SerializeField] private GameObject confirmText;
+
+	/// <summary>
+	/// Animates the unique elements of this form into view
+	/// </summary>
+	protected override void AnimateUniqueElementsIn()
+	{
+		questionText.AnimateScaleX(1f, 0.15f);
+		line.AnimateScaleX(1f, 0.175f);
+		purposeNameText.AnimateGraphicAndScale(1f, 1f, 0.2f);
+		prpsIcon.AnimateGraphicAndScale(1f, 1f, 0.225f);
+		currentPRPSText.AnimateGraphicAndScale(1f, 1f, 0.25f);
+		additionPRPSText.AnimateGraphicAndScale(1f, 1f, 0.275f);
+
+		if (confirmText.activeInHierarchy)
+		{
+			confirmText.AnimateGraphicAndScale(1f, 1f, 0.3f, FinishedAnimating);
+		}
+		else
+		{
+			confirmButton.AnimateGraphicAndScale(1f, 1f, 0.3f);
+			cancelButton.AnimateGraphicAndScale(1f, 1f, 0.3f, FinishedAnimating);
+		}
+	}
+}
---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Popups/Concrete/OkCancelPopup/Concrete/ConfirmReleasePopup.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// Class used for displaying the PRPS release amounts on the ConfirmReleasePopup
+/// </summary>
+public sealed class ConfirmReleasePopup : ConfirmTransactionPopupBase<ConfirmReleasePopup>
+{
+	[SerializeField] private TextMeshProUGUI questionText;
+
+	// If user does not have enough ETH to release purpose, set the questionText
+	// to "Not enough ETH" and change the text colour to UIColors.Red and
+	// set the okButton.interactable = false;
+
+	/// <summary>
+	/// Displays the asset transfer request details.
+	/// </summary>
+	/// <param name="transactionInput"> The input of the send asset transaction request. </param>
+	protected override void InternalSetConfirmationValues(object[] transactionInput)
+	{
+
+	}
+}
