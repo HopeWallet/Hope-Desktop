f9169334be850448d298ff0e463680d08be8098c
Matthew LeLiever <matthew121250@hotmail.com>
2018-10-16 20:20:55 -0400

    Added PlayerPrefConstants

---
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/Data/Constants/PlayerPrefConstants.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// Class containing player pref key constants.
+/// </summary>
+public static class PlayerPrefConstants
+{
+    public const string PREVIOUS_ACCOUNT_NUM = "previous_account_num";
+    public const string PREVIOUS_DERIVATION_PATH = "previous_derivation_path";
+}
