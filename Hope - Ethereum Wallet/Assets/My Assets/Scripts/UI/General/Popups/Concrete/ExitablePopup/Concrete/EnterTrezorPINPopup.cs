a4ee3590f6e27117d4d63c127f51a27e3d0852c9
Matthew LeLiever <matthew121250@hotmail.com>
2018-10-22 12:56:18 -0400

    Implementing trezor pin popup

--- Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Popups/Concrete/ExitablePopup/Concrete/EnterTrezorPINPopup.cs
+++ Hope - Ethereum Wallet/Assets/My Assets/Scripts/UI/General/Popups/Concrete/ExitablePopup/Concrete/EnterTrezorPINPopup.cs
@@ -1,3 +1,21 @@
-﻿public sealed class EnterTrezorPINPopup : ExitablePopupComponent<EnterTrezorPINPopup>
+﻿using Zenject;
+
+public sealed class EnterTrezorPINPopup : ExitablePopupComponent<EnterTrezorPINPopup>
 {
+    private TrezorWallet trezorWallet;
+
+    private bool pinSectionOpen;
+
+    public TrezorPINSection TrezorPINSection { get; private set; }
+
+    [Inject]
+    public void Construct(TrezorWallet trezorWallet)
+    {
+        this.trezorWallet = trezorWallet;
+    }
+
+    protected override void OnStart()
+    {
+        TrezorPINSection = GetComponentInChildren<TrezorPINSection>();
+    }
 }
